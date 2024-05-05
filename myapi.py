cd59fe259f0b2434e66377b08f66e8c9e9bcf9e3
chima <chimaemekaiheonu@gmail.com>
2024-05-05 15:15:54 +0100

    new change

--- myapi.py
+++ myapi.py
@@ -46,4 +46,8 @@ def create_student(student_id : int , student : Student):
 
 @app.put('/update-student/{student_id}')
 def update_students(student_id:int, student:UpdateStudent):
+    if student_id not in students:
+        return{"Error: Student does not exist"}
     
+    students[student_id] = student
+    return students[student_id]
